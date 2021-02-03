2bbcc9503f5668372f7ee8068db67ff92be747c1
amadejpapez <63184600+amadejpapez@users.noreply.github.com>
2021-02-03 20:22:06 +0100

    Version 2.1

--- ApplSec.py
+++ ApplSec.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup, SoupStrainer
 
 
 updatesPage = requests.get("https://support.apple.com/en-us/HT201222").text
-# currentDate = str(date.today().day) + " " + str(date.today().strftime('%B')[0:3]) + " " + str(date.today().year)
+# currentDate = str(date.today().day) + " " + str(date.today().strftime("%B")[0:3]) + " " + str(date.today().year)
 currentDate = str(1) + " " + "Feb" + " " + str(date.today().year)
 
 
@@ -21,11 +21,11 @@ if (wasUpdated == True):
     # if there was an update scrape all the links
     allLinks = []
     http = httplib2.Http()
-    status, response = http.request('https://support.apple.com/en-us/HT201222')
+    status, response = http.request("https://support.apple.com/en-us/HT201222")
 
-    for link in BeautifulSoup(response, features="html.parser", parse_only=SoupStrainer('a')):
-        if link.has_attr('href'):
-            allLinks.append(link['href'])
+    for link in BeautifulSoup(response, features="html.parser", parse_only=SoupStrainer("a")):
+        if link.has_attr("href"):
+            allLinks.append(link["href"])
 
     # get only the new links
     searchResults = len(re.findall(currentDate, updatesPage)) + 22
@@ -37,10 +37,10 @@ if (wasUpdated == True):
 
     for x in newLinks:
         newLink = requests.get(x)
-        soup = BeautifulSoup(newLink.content, 'html.parser')
+        soup = BeautifulSoup(newLink.content, "html.parser")
 
         # get the header of the new version
-        allHeaders = soup.find_all('h2')
+        allHeaders = soup.find_all("h2")
         newHeader.append(re.sub("<[^>]*?>","", str(allHeaders[1])))
 
         # search how many CVEs there are
@@ -50,10 +50,12 @@ if (wasUpdated == True):
         # if there is macOS in the header take only the first part, not the full header
         if "macOS" in x:
             macosIsHere = newHeader.index(x)
-            newHeader[macosIsHere] = x.split(',', 1)[0]
+            newHeader[macosIsHere] = x.split(",", 1)[0]
 
     # print results
-    print("PATCH TIME!")
+    results = "PATCH TIME!\n"
     for x in newHeader:
-        print(x + " released with " + str(numberCVE[0]) + " security fixes")
+        results += x + " released with " + str(numberCVE[0]) + " security fixes\n"
         numberCVE.pop(0)
+
+    print(results)
