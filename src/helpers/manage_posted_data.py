f4b26a683ecce0d9ac915a1006d9abe4b220b60f
amadejpapez <amadej.papez@icloud.com>
2023-06-09 20:19:35 +0200

    ref: create PostedFile class, share file contents between modules
    This commit creates a new class, which stores data for the
    posted_data.json in a class variable.
    
    This allows for easy data reading and modifying between functions,
    modules and similar, without passing it as a parameter or reading/saving.

--- src/helpers/manage_posted_data.py
+++ src/helpers/manage_posted_data.py
@@ -1,67 +1,70 @@
 """
-Functions for saving and reading data from src/posted_data.json
+Save and read data in src/posted_data.json
 
-File is storing all of the recent posts. It also contains last 10 zero-days,
-which is used for saying "new" or "additional" patch. Alongside it stores all of
-the releases, for which Apple says "no details yet".
+File is used to store:
+    - what was posted today
+    - 10 last zero-day CVEs, for "new" and "additional" patch text
+    - releases for which Apple says "no details yet"
 """
+
+from __future__ import annotations
 
 import json
 import os
 
 import helpers.get_date as get_date
 
-LOC = os.path.abspath(os.path.join(__file__, "../../posted_data.json"))
 
-FILE_STRUCTURE = {
-    "zero_days": [],
-    "details_available_soon": [],
-    "posts": {
-        "new_releases": [],
-        "new_sec_content": [],
-        "ios_modules": [],
-        "zero_days": {},
-        "yearly_report": [],
-    },
-}
+class PostedFile:
+    _LOC = os.path.abspath(os.path.join(__file__, "../../posted_data.json"))
+    FILE_STRUCTURE = {
+        "zero_days": [],
+        "details_available_soon": [],
+        "posts": {
+            "new_releases": [],
+            "new_sec_content": [],
+            "ios_modules": [],
+            "zero_days": {},
+            "yearly_report": [],
+        }
+    }
+    # data is a class variable, so that it does not have to be saved/read between different modules
+    data: dict
 
+    @staticmethod
+    def read():
+        try:
+            with open(PostedFile._LOC, "r", encoding="utf-8") as json_file:
+                PostedFile.data = json.load(json_file)
+        except Exception:
+            PostedFile.data = PostedFile.FILE_STRUCTURE
+            PostedFile.save()
 
-def read() -> dict:
-    try:
-        with open(LOC, "r", encoding="utf-8") as json_file:
-            posted_data_json = json.load(json_file)
+        assert PostedFile.data["posts"]["new_sec_content"] != [], "ERROR: 'new_sec_content' list inside of posted_data.json is empty. This is used to recognize that releases published before them are new. Add at least last 3 release names there."
 
-    except (json.JSONDecodeError, FileNotFoundError):
-        save(FILE_STRUCTURE)
-        posted_data_json = read()
+        assert PostedFile.data["posts"]["new_releases"] != [], "ERROR: 'new_releases' list inside of posted_data.json is empty. This is used to recognize that releases published before them are new. Add at least last 3 release names there."
 
-    assert posted_data_json["posts"]["new_sec_content"] != [], "ERROR: 'new_sec_content' list inside of posted_data.json is empty. This is used to recognize that releases published before them are new. Add at least last 3 release names there."
+    @staticmethod
+    def save() -> None:
+        with open(PostedFile._LOC, "w", encoding="utf-8") as json_file:
+            json.dump(PostedFile._clear_old_data(PostedFile.data), json_file, indent=4)
 
-    assert posted_data_json["posts"]["new_releases"] != [], "ERROR: 'new_releases' list inside of posted_data.json is empty. This is used to recognize that releases published before them are new. Add at least last 3 release names there."
+    @staticmethod
+    def _clear_old_data(new_data: dict) -> dict:
+        while len(new_data["posts"]["new_releases"]) > 15:
+            new_data["posts"]["new_releases"].pop(0)
 
-    return posted_data_json
+        while len(new_data["posts"]["new_sec_content"]) > 15:
+            new_data["posts"]["new_sec_content"].pop(0)
 
+        while len(new_data["posts"]["ios_modules"]) > 3:
+            new_data["posts"]["ios_modules"].pop(0)
 
-def clear_old_data(new_data: dict) -> dict:
-    while len(new_data["posts"]["new_releases"]) > 15:
-        new_data["posts"]["new_releases"].pop(0)
+        while len(new_data["zero_days"]) > 10:
+            new_data["zero_days"].pop(0)
 
-    while len(new_data["posts"]["new_sec_content"]) > 15:
-        new_data["posts"]["new_sec_content"].pop(0)
+        if get_date.is_midnight():
+            new_data["posts"]["zero_days"] = {}
+            new_data["posts"]["yearly_report"] = []
 
-    while len(new_data["posts"]["ios_modules"]) > 3:
-        new_data["posts"]["ios_modules"].pop(0)
-
-    while len(new_data["zero_days"]) > 10:
-        new_data["zero_days"].pop(0)
-
-    if get_date.is_midnight():
-        new_data["posts"]["zero_days"] = {}
-        new_data["posts"]["yearly_report"] = []
-
-    return new_data
-
-
-def save(new_data: dict) -> None:
-    with open(LOC, "w", encoding="utf-8") as json_file:
-        json.dump(clear_old_data(new_data), json_file, indent=4)
+        return new_data
